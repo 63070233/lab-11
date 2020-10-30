26c40fbc40a5c1203c8787bb544e3b517055be5b
baspop01 <73678928+baspop01@users.noreply.github.com>
2020-10-30 10:55:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <img src="https://th.virbac.com/files/live/sites/virbac-th/files/predefined-files/species/pictures/Cats/Cat%20species%202%20-%20280x205.jpg">
         <br>
         <p>A : คืนก่อนครับ</p>
-        <p>B : ทำไมครับ คุณจะเล่าให้ผมฟังว่าคืนก่อนคุณพาแฟนไปเที่ยวสินะ/p>
+        <p>B : ทำไมครับ คุณจะเล่าให้ผมฟังว่าคืนก่อนคุณพาแฟนไปเที่ยวสินะ</p>
         <p>A : ไม่ครับ เงินที่คุณยืมผมไปอะ คืนก่อนครับ กูจะแย่งอาหารแมวแดกแล้วครับ</p>  
     </body>
 </html>
