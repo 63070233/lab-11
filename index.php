f36a573d1629ec2ce4cc8e136276e498306a3289
baspop01 <73678928+baspop01@users.noreply.github.com>
2020-10-30 10:49:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 echo "<h1>พัชรพล ศาศวัตเตชะ</h1>";
+<img src="https://ichef.bbci.co.uk/news/640/cpsprodpb/51F3/production/_106997902_gettyimages-611696954.jpg">;
+<img src="https://th.virbac.com/files/live/sites/virbac-th/files/predefined-files/species/pictures/Cats/Cat%20species%202%20-%20280x205.jpg">;
 echo "<h1>A : คืนก่อนครับ</h1>";
 echo "<h1>B : ทำไมครับ คุณจะเล่าให้ผมฟังว่าคืนก่อนคุณพาแฟนไปเที่ยวสินะ</h1>";
 echo "<h1>A : ไม่ครับ เงินที่คุณยืมผมไปอะ คืนก่อนครับ กูจะแย่งอาหารแมวแดกแล้วครับ</h1>";
