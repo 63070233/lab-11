2d109621d0cce95353fd60ef81bca59956c554ce
baspop01 <73678928+baspop01@users.noreply.github.com>
2020-11-06 15:04:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <html>
-   
-<head>
+   <head>
     <title>พัชรพล ศาศวัตเตชะ 63070115</titile>
-</head>
-    
-<body>
+   </head>
+   <body>
     <a href = "photo.html"> Work1 6/11/20</a><br>
     <a href = "Card.html"> Work2 6/11/20</a>
-
- 
-</body>
-
+   </body>
 </html>
