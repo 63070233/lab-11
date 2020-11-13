a8246db518a72065faf01c6a6e97e9c84c65ddf6
baspop01 <73678928+baspop01@users.noreply.github.com>
2020-11-13 10:20:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
    </head>
    <body>
     <a href = "photo.html"> Work1 6/11/20</a><br>
-    <a href = "Card.html"> Work2 6/11/20</a>
+    <a href = "Card.html"> Work2 6/11/20</a><br>
     <a href = "show.php"> database 13/11/20</a>
    </body>
 </html>
