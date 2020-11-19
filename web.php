0250c031831d932d789a05f5b5449be5155f04fe
baspop01 <73678928+baspop01@users.noreply.github.com>
2020-11-19 20:47:54 +0700

    Update and rename index.php to web.php

--- web.php
+++ web.php
@@ -5,6 +5,6 @@
    <body>
     <a href = "photo.html"> Work1 6/11/20</a><br>
     <a href = "Card.html"> Work2 6/11/20</a><br>
-    <a href = "show.php"> database 13/11/20</a><br>
+    <a href = "index.php"> database 13/11/20</a><br>
    </body>
 </html>
