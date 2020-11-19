c6c0148b6e63deaa96892015c997d07ebfb3e47b
baspop01 <73678928+baspop01@users.noreply.github.com>
2020-11-19 20:20:02 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,12 +1,7 @@
-  
-<?php
-	$conn = mysqli_real_connect($conn, 'databasebas.mysql.database.azure.com', 'baspop01@databasebas', 'Phatcharapon123', 'ITFLAB', 3306);
-	$sql = 'DELETE FROM guestbook WHERE ID = '.$_GET['ID'].'';
-?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Delete | 13th ITF LAB</title>
+	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
 	<style type="text/css">
@@ -23,15 +18,16 @@
 					<div class="card-body">
 						<h2 align="center">
 						<?php
-							if(mysqli_query($conn, $sql)) {
-								echo "DELETE COMPLETED";
+							$conn = mysqli_real_connect($conn, 'databasebas.mysql.database.azure.com', 'baspop01@databasebas', 'Phatcharapon123', 'ITFLAB', 3306);
+							if (mysqli_query($conn, 'DELETE FROM guestbook WHERE ID = '.$_GET['ID'].'')) {
+								echo 'DELETE COMPLETED';
 							}
 							else {
-								echo "FAILED TO DELETE";
+								echo 'ERROR!';
 							}
 						?>
 						</h2>
-						<p align="center" class="mt-4 mb-0"><a href="show.php" class="btn btn-sm btn-warning">BACK</a></p>
+						<p align="center"><a href="show.php" class="btn btn-sm btn-dark">BACK</a></p>
 					</div>
 				</div>
 			</div>
@@ -39,4 +35,3 @@
 	</div>
 </body>
 </html>
-
