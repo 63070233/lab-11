a9ddb390876464fb5a1da87841a94840e9b89f91
baspop01 <73678928+baspop01@users.noreply.github.com>
2020-11-27 09:59:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
     <a href = "photo.html"> Work1 6/11/20</a><br>
     <a href = "Card.html"> Work2 6/11/20</a><br>
     <a href = "itflab/index.php"> database 13/11/20</a><br>
+      <a href = "labexam/index.php"> lab exam</a><br>
    </body>
 </html>
